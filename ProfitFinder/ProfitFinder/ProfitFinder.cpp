9af27eb7b9f2bcc1d03cadaf71fca7e48ef73e87
VulpoTheDev <86149507+VulpoTheDev@users.noreply.github.com>
2021-06-20 01:14:27 +0000

    Lesson 1
    Study and create the Chapter 2 Tying It All Together: Smile! program in Visual Studio 2015. Do NOT simply copy and paste the code from a digital source. You can only learn proper coding by TYPING it out. That way, you see each line of code and it can be placed in your long-term memory. Remember, the objective is to LEARN how to program, not simply complete the exercises as fast as possible.
    Assignment 3 - Question 3
    
    An electronics company makes circuit boards that cost $14.95 apiece to produce. Write a program to determine how much the company should sell them for if it wants to make a 35 percent profit. Display the result on the screen.

---
+++ ProfitFinder/ProfitFinder/ProfitFinder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double circutBoardPrice = 14.95, profitPercent = 0.35;
+    cout << "Circut Board price: " << circutBoardPrice << endl;
+    cout << "Profit Percent: " << profitPercent << endl;
+    cout << "You'll need to sell them for " << 14.95 * 0.35 << " each if you're wanting to make a " << profitPercent << " for each circut board";
+
+}
+
---
+++ twoSmileFace/ConsoleApplication1/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// This Program will print 2 faces
+int main() {
+	cout << endl << endl;
+	cout << "     O      O       ^     ^" << endl;
+	cout << "        o           V     V" << endl;
+	cout << "     (______)          :" << endl;
+	cout << "                     \\UUU/" << endl;
+	return 0;
+}
